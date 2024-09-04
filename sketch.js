87752fa9ff5a69cafa55d1cd4c66f4bbca88245c
GatoMiau03 <d.benaldo03@ufromail.cl>
2024-09-04 09:46:28 -0400

    creacion sketch.js / carpeta imagenes

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let classifier;
+let img;
+let label = "";
+let confidence = "";
+
+
+function preload() {
+    classifier = ml5.imageClassifier("MobileNet");
+    img = loadImage("images/azul.png")
+}
+
+function setup() {
+    createCanvas(400, 400);
+    classifier.classify(img, gotResult);
+    image(img, 0, 0, width, height);
+  }
+
+function draw() {
+    background(220);
+}
+
+function gotResult(results) {
+    console.log(results);
+    fill(255);
+    stroke(0);
+    textSize(18);
+    label = "Label: " + results[0].label;
+    confidence = "Confidence: " + nf(results[0].confidence, 0, 2);
+    text(label, 10, 360);
+    text(confidence, 10, 380);
+}
